afbaed20e3762fb51ca4862a73140287de1a839b
raguaman55 <raguaman5@utpl.edu.ec>
2019-10-24 20:37:45 -0500

    Practica en clase

---
+++ PracticaJAVA/Asignaciones2.java
@@ -0,0 +1,14 @@
+public class Asignaciones {
+	public static void main(String[] args) {
+		int x;
+		int y;
+		int sum, mul;
+		x = 2;
+		y = 9;
+		sum = x + y;
+		mul = x * y;
+		System.out.println("La suma de mis variables es " + sum);
+		System.out.println("El producto de mis variables es " + mul);
+	}
+	
+}
---
+++ PracticaJAVA/Saludo1.java
@@ -0,0 +1,8 @@
+public class Saludo1 {
+	public static void main (String[] args) {
+		String nombre;
+		nombre = "Juan";
+		System.out.println("Saludos" + nombre + "!");
+
+	}
+}
